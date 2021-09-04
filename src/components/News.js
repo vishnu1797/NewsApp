1dca5abcdbdc3ab9497b411a62210f5c0dd066d3
vishnu1797 <vishnusinghc24@gmail.com>
2021-09-04 12:42:48 +0530

    Adding categories and refactoring news component of our news app

--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem';
-import Spinner from './Spinner'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+
 
 export class News extends Component {
+
+    static defaultProps = {
+            pageSize: 8,
+            country: 'in',
+            category:'science'
+    }
+
+    static propTypes = {
+        pageSize: PropTypes.number,
+        country: PropTypes.string,
+        category: PropTypes.string,
+    }
     constructor() {
         super();
         this.state = {
@@ -12,8 +26,9 @@ export class News extends Component {
         }
     }
 
-    async componentDidMount() {
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=bd4354274b0f402d9829f49b956a3346&page=1&pageSize=${this.props.pageSize}`;
+    // Creating a function upDate news which we can use in our required functions
+    async upDateNews(){
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=819bf47e4f8f4e9fa0ae40b82705567c&page=${this.props.page}&pageSize=${this.props.pageSize}`;
         this.setState({loading: true})
         let data = await fetch(url);
         let parsedData = await data.json();
@@ -21,33 +36,26 @@ export class News extends Component {
         this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults, loading:false });
     }
 
+    //This function will run after the render function
+    async componentDidMount() {
+        this.upDateNews();
+    }
+
+    //This is the previous button function
     handleOnPrev = async () => {
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=bd4354274b0f402d9829f49b956a3346&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-        this.setState({loading: true})
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        console.log(parsedData);
+        this.upDateNews();
         this.setState({
             page: this.state.page - 1,
-            articles: parsedData.articles,
-            loading:false
         })
     }
 
+        //This is the next button function
     handleOnNext = async () => {
-        if (this.state.page + 1 > Math.ceil(this.state.page.totalResults / this.props.pageSize)) {
-
-        }
+        if (this.state.page + 1 > Math.ceil(this.state.page.totalResults / this.props.pageSize)) {}
         else {
-            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=bd4354274b0f402d9829f49b956a3346&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-            this.setState({loading: true})
-            let data = await fetch(url);
-            let parsedData = await data.json();
-            console.log(parsedData);
+            this.upDateNews();
             this.setState({
                 page: this.state.page + 1,
-                articles: parsedData.articles,
-                loading: false
             })
         }
     }
@@ -60,13 +68,14 @@ export class News extends Component {
                     <h2 className="text-center"><b>Top headlines - created with ReactJS</b></h2><hr /><br />
                    {this.state.loading && <Spinner/>}  
                     <div className="row">
-                        { !this.state.loading&&this.state.articles.map((element) => {
+                        { !this.state.loading && this.state.articles.map((element) => {
                             return <div key={element.url} className="col-md-4">
-                                <NewsItem title={element.title} description={element.description} newsUrl={element.url} imageUrl={element.urlToImage} />
+                                <NewsItem title={element.title} description={element.description} newsUrl={element.url} imageUrl={element.urlToImage} author={!element.author? "unknown":element.author} publishedAt={new Date (element.publishedAt).toGMTString()} source = {element.source.name} />
                             </div>
                         })
                         }
                     </div>
+                    {/* //Adding buttons */}
                     <div className="container d-flex justify-content-between">
                         <button disabled={this.state.page <= 1} type="button" className="btn btn-info" onClick={this.handleOnPrev}>&larr;Previous</button>
                         <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-info" onClick={this.handleOnNext}>Next&rarr;</button>
